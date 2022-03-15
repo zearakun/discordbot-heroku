a1e8f4a94e95b5030888221f0c232163e4dcd0c1
zearakun <91083604+zearakun@users.noreply.github.com>
2022-03-15 21:50:14 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import discord
+
+
+prefix = "プレフィックス"
+
+client = commands.Bot(command_prefix=prefix)
+
+token = "トークン"
+
+@client.command()
+async def ping(ctx):
+    embed = discord.Embed(title=f"pong!:ping_pong:", color=0x979c9f)
+    embed.add_field(name="ping",
+                    value=f"{client.latency*1000}ms",
+                    inline=False)
+    embed.set_footer(icon_url=ctx.author.avatar_url, text=ctx.author)
+    embed.timestamp = datetime.datetime.utcnow()
+    await ctx.reply(embed=embed)
+
+
+client.run(token)
