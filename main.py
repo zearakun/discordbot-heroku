a9d545328b632e06b7c452a132f3852fc5efe89a
zearakun <91083604+zearakun@users.noreply.github.com>
2022-03-15 22:28:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,5 +14,16 @@ client = commands.Bot(command_prefix=prefix)
 async def on_ready():
     print("discord.py version" + discord.__version__)
 
+    
+@client.command()
+async def ping(ctx):
+    embed = discord.Embed(title=f"pong!:ping_pong:", color=0x979c9f)
+    embed.add_field(name="ping",
+                    value=f"{client.latency*1000}ms",
+                    inline=False)
+    embed.set_footer(icon_url=ctx.author.avatar_url, text=ctx.author)
+    embed.timestamp = datetime.datetime.utcnow()
+    await ctx.reply(embed=embed)
+    
 
 client.run(token)
